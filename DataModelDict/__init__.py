ac4df307f3f598837195dafc865fc8601eafca05
Lucas Hale <lucas.hale@nist.gov>
2023-07-28 16:39:28 -0400

    fix for depreciation

--- DataModelDict/__init__.py
+++ DataModelDict/__init__.py
@@ -3,7 +3,10 @@ from importlib import resources
 __all__ = ['DataModelDict', 'uber_open_rmode', 'parsepath', 'joinpath']
 
 # Read version from VERSION file
-__version__ = resources.read_text('DataModelDict', 'VERSION').strip()
+if hasattr(resources, 'files'):
+    __version__ = resources.files('DataModelDict').joinpath('VERSION').read_text(encoding='UTF-8')
+else:
+    __version__ = resources.read_text('DataModelDict', 'VERSION', encoding='UTF-8').strip()
 
 # Local imports
 from .uber_open_rmode import uber_open_rmode
