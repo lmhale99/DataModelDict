a14869f67445881562261f5dbaa04a5792889428
Lucas Hale <lucas.hale@nist.gov>
2026-07-23 13:34:42 -0400

    Build updated to support uv

--- DataModelDict/__init__.py
+++ DataModelDict/__init__.py
@@ -1,15 +1,9 @@
-# coding: utf-8
-from importlib import resources
-__all__ = ['DataModelDict', 'uber_open_rmode', 'parsepath', 'joinpath']
+from importlib.metadata import version
+__version__ = version('DataModelDict')
 
-# Read version from VERSION file
-if hasattr(resources, 'files'):
-    __version__ = resources.files('DataModelDict').joinpath('VERSION').read_text(encoding='UTF-8')
-else:
-    __version__ = resources.read_text('DataModelDict', 'VERSION', encoding='UTF-8').strip()
-
-# Local imports
 from .uber_open_rmode import uber_open_rmode
 from .parsepath import parsepath
 from .joinpath import joinpath
 from .DataModelDict import DataModelDict
+
+__all__ = ['DataModelDict', 'uber_open_rmode', 'parsepath', 'joinpath']
